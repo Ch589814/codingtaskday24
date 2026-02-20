4773b6dd0672ebdb81d194074789026fc1bbcb50
Ch589814 <niyomufashadelphine1@gmail.com>
2026-02-20 09:44:15 +0200

    Add FunctionalCalculator with BiFunction-based calculate method

--- src/FunctionalCalculator.java
+++ src/FunctionalCalculator.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+import java.util.function.BiFunction;
 
 public class FunctionalCalculator {
+
+    public static Integer calculate(int num1, int num2, BiFunction<Integer, Integer, Integer> operation) {
+        return operation.apply(num1, num2);
+    }
 }
